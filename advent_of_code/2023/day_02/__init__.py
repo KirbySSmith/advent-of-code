451784eac7534d8ebb4062e24d819f41264ed9f0
KirbySSmith <KirbySSmith@users.noreply.github.com>
2023-12-02 10:11:31 -0500

    Advent of code day two part two

--- advent_of_code/2023/day_02/__init__.py
+++ advent_of_code/2023/day_02/__init__.py
@@ -1,30 +1,26 @@
 values_file = open('values.txt', 'r')
 values = values_file.readlines()
 
-max_cubes = {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-}
-
-
-game_number_sum = 0
+game_power_sum = 0
 for game in values:
+    max_cubes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+
     game_split = str.split(game, ':')
     game_number = game_split[0][5::]
     hand_split = str.split(game_split[1], ';')
-    valid_game = True
     for hand in hand_split:
-        if valid_game is True:
-            cubes_split = str.split(hand, ',')
-            for cubes in cubes_split:
-                count_color = str.split(str.strip(cubes), " ")
-                count = count_color[0]
-                color = count_color[1]
-                if max_cubes[color] < int(count):
-                    valid_game = False
-                    break
-    if valid_game is True:
-        game_number_sum += int(game_number)
+        cubes_split = str.split(hand, ',')
+        for cubes in cubes_split:
+            count_color = str.split(str.strip(cubes), " ")
+            count = int(count_color[0])
+            color = count_color[1]
+            if max_cubes[color] < count:
+                max_cubes[color] = count
+    game_power = max_cubes["red"] * max_cubes["green"] * max_cubes["blue"]
+    game_power_sum += game_power
 
-print(f'answer is {game_number_sum}')
+print(f'answer is {game_power_sum}')
