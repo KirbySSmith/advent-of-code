4945e0bf9c4e5bc0a54abc4cc15fc9f9f09690b0
KirbySSmith <KirbySSmith@users.noreply.github.com>
2023-12-03 15:46:11 -0500

    Advent of code day three part two

--- advent_of_code/2023/day_03/__init__.py
+++ advent_of_code/2023/day_03/__init__.py
@@ -2,7 +2,6 @@ values_file = open('values.txt', 'r')
 values = values_file.readlines()
 
 parts_sum = 0
-part_number_positions = set()
 
 
 def find_number_start_x_y(x, y):
@@ -12,9 +11,19 @@ def find_number_start_x_y(x, y):
     return x - (offset - 1), y
 
 
+def get_number(position):
+    x = position[0]
+    y = position[1]
+    offset = 0
+    while values[y][x + offset].isdigit():
+        offset += 1
+    return int(values[y][x:x+offset:])
+
+
 for y, line in enumerate(values):
     for x, char in enumerate(line.strip()):
-        if not char.isdigit() and char != '.':
+        if char == '*':
+            part_number_positions = set()
             if y > 0:
                 if x > 0 and values[y-1][x-1].isdigit():
                     part_number_positions.add(find_number_start_x_y(x-1, y-1))
@@ -36,9 +45,7 @@ for y, line in enumerate(values):
                 if x != len(line) - 1 and values[y+1][x+1].isdigit():
                     part_number_positions.add(find_number_start_x_y(x+1, y+1))
 
-for x, y in part_number_positions:
-    offset = 0
-    while values[y][x + offset].isdigit():
-        offset += 1
-    parts_sum += int(values[y][x:x+offset:])
+            if len(part_number_positions) == 2:
+                parts_sum += (get_number(part_number_positions.pop()) * get_number(part_number_positions.pop()))
+
 print(parts_sum)
