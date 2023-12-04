fe4e9f917e4b78a8e2223b3cbc663add523aac8d
KirbySSmith <KirbySSmith@users.noreply.github.com>
2023-12-04 10:29:32 -0500

    Advent of code day four part two

--- advent_of_code/2023/day_04/__init__.py
+++ advent_of_code/2023/day_04/__init__.py
@@ -1,7 +1,10 @@
 values_file = open('values.txt', 'r')
 values = values_file.readlines()
 
-card_score_sum = 0
+card_count = 0
+card_scores = []
+tree_results = [None] * len(values)
+
 for card in values:
     card_split = str.split(card, ':')
     numbers_split = str.split(card_split[1], '|')
@@ -10,8 +13,27 @@ for card in values:
 
     matching_numbers = your_numbers.intersection(winning_numbers)
     matching_count = len(matching_numbers)
-    if matching_count != 0:
-        card_score = 1 if matching_count == 1 else 2 ** (matching_count - 1)
-        card_score_sum += card_score
+    card_scores.append(matching_count)
 
-print(f'answer is {card_score_sum}')
+
+def process_card_tree(current_index: int):
+    offset = 0
+    result_card_count = 1
+    while offset < card_scores[current_index]:
+        offset += 1
+        next_index = current_index + offset
+        tree_result = tree_results[next_index]
+        if tree_result is None:
+            tree_result = process_card_tree(next_index)
+            tree_results[next_index] = tree_result
+        result_card_count += tree_result
+
+    return result_card_count
+
+
+index = 0
+while index < len(card_scores):
+    card_count += process_card_tree(index)
+    index += 1
+
+print(f'answer is {card_count}')
